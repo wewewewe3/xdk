20c5eb0375544db48060bfec1a7fb31ed026f5b1
yasser <yasser@ymail.com>
2015-06-26 03:06:39 +0300

    up
    up

---
+++ js/index_user_scripts.js
@@ -0,0 +1,313 @@
+(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+     /* button  Home */
+    
+    
+        /* button  Favourites */
+    
+    
+        /* button  messages */
+    
+    
+        /* button  Profile */
+    $(document).on("click", ".uib_w_4", function(evt)
+    {
+         activate_subpage("#uib_page_5"); 
+    });
+    
+        /* button  النباتات */
+    $(document).on("click", ".uib_w_13", function(evt)
+    {
+         activate_subpage("#nbatgrid"); 
+    });
+    
+        /* button  النباتات */
+    $(document).on("click", ".uib_w_47", function(evt)
+    {
+         activate_subpage("#nbatgrid"); 
+    });
+    
+        /* graphic button  قرّيص */
+    
+    
+        /* graphic button  قرّيص */
+    
+    
+        /* graphic button  قرّيص */
+    $(document).on("click", ".uib_w_15", function(evt)
+    {
+         activate_subpage("#qrees"); 
+    });
+    
+        /* button  الصور */
+    $(document).on("click", ".uib_w_12", function(evt)
+    {
+         activate_subpage("#imag"); 
+    });
+    
+        /* button  الصور */
+    
+    
+        /* button  الرئيسية */
+    $(document).on("click", ".uib_w_6", function(evt)
+    {
+         activate_subpage("#uib_page_2"); 
+    });
+    
+        /* button  النباتات */
+    $(document).on("click", ".uib_w_7", function(evt)
+    {
+         activate_subpage("#nbatgrid"); 
+    });
+    
+        /* graphic button  بختري */
+    $(document).on("click", ".uib_w_16", function(evt)
+    {
+         activate_subpage("#b5try"); 
+    });
+    
+        /* graphic button  بروق */
+    $(document).on("click", ".uib_w_17", function(evt)
+    {
+         activate_subpage("#brooq"); 
+    });
+    
+        /* graphic button  حمطّ */
+    $(document).on("click", ".uib_w_18", function(evt)
+    {
+         activate_subpage("#h7m6"); 
+    });
+    
+        /* graphic button  بسّباس */
+    $(document).on("click", ".uib_w_19", function(evt)
+    {
+         activate_subpage("#bsbas"); 
+    });
+    
+        /* graphic button  بصل البر */
+    $(document).on("click", ".uib_w_20", function(evt)
+    {
+         activate_subpage("#b9lalbr"); 
+    });
+    
+        /* graphic button  بعيثران */
+    $(document).on("click", ".uib_w_21", function(evt)
+    {
+         activate_subpage("#b3ythran"); 
+    });
+    
+        /* graphic button  ثندة */
+    $(document).on("click", ".uib_w_22", function(evt)
+    {
+         activate_subpage("#thndh"); 
+    });
+    
+        /* graphic button  جرجير */
+    $(document).on("click", ".uib_w_23", function(evt)
+    {
+         activate_subpage("#jrjeer"); 
+    });
+    
+        /* graphic button  جعّد */
+    $(document).on("click", ".uib_w_24", function(evt)
+    {
+         activate_subpage("#j3d"); 
+    });
+    
+        /* graphic button  حزآ */
+    $(document).on("click", ".uib_w_25", function(evt)
+    {
+         activate_subpage("#h7za"); 
+    });
+    
+        /* graphic button  حمباز */
+    $(document).on("click", ".uib_w_26", function(evt)
+    {
+         activate_subpage("#h7mbaz"); 
+    });
+    
+        /* graphic button  حمبصيص */
+    $(document).on("click", ".uib_w_27", function(evt)
+    {
+         activate_subpage("#h7mb9e9"); 
+    });
+    
+        /* graphic button  حميض */
+    $(document).on("click", ".uib_w_28", function(evt)
+    {
+         activate_subpage("#h7meed"); 
+    });
+    
+        /* graphic button  حنظل */
+    $(document).on("click", ".uib_w_29", function(evt)
+    {
+         activate_subpage("#h7ndal"); 
+    });
+    
+        /* graphic button  حوآ */
+    $(document).on("click", ".uib_w_30", function(evt)
+    {
+         activate_subpage("#hwa"); 
+    });
+    
+        /* graphic button  حوذان */
+    $(document).on("click", ".uib_w_31", function(evt)
+    {
+         activate_subpage("#h7othan"); 
+    });
+    
+        /* graphic button  خبيز */
+    $(document).on("click", ".uib_w_32", function(evt)
+    {
+         activate_subpage("#k5bez"); 
+    });
+    
+        /* graphic button  ربحلا */
+    $(document).on("click", ".uib_w_33", function(evt)
+    {
+         activate_subpage("#rb7ala"); 
+    });
+    
+        /* graphic button  رجلة */
+    $(document).on("click", ".uib_w_34", function(evt)
+    {
+         activate_subpage("#rglah"); 
+    });
+    
+        /* graphic button  رخام */
+    $(document).on("click", ".uib_w_35", function(evt)
+    {
+         activate_subpage("#r5am"); 
+    });
+    
+        /* graphic button  رشاد */
+    $(document).on("click", ".uib_w_36", function(evt)
+    {
+         activate_subpage("#rshad"); 
+    });
+    
+        /* graphic button  زعلوق */
+    $(document).on("click", ".uib_w_37", function(evt)
+    {
+         activate_subpage("#z3looq"); 
+    });
+    
+        /* graphic button  سدر */
+    $(document).on("click", ".uib_w_38", function(evt)
+    {
+         activate_subpage("#sder"); 
+    });
+    
+        /* graphic button  سعدان */
+    $(document).on("click", ".uib_w_39", function(evt)
+    {
+         activate_subpage("#s3dan"); 
+    });
+    
+        /* graphic button  شحوم */
+    $(document).on("click", ".uib_w_40", function(evt)
+    {
+         activate_subpage("#sh7oom"); 
+    });
+    
+        /* graphic button  شدق الجمل */
+    $(document).on("click", ".uib_w_41", function(evt)
+    {
+         activate_subpage("#shdq"); 
+    });
+    
+        /* graphic button  شفلح */
+    $(document).on("click", ".uib_w_42", function(evt)
+    {
+         activate_subpage("#shfl7"); 
+    });
+    
+        /* graphic button  شيح */
+    $(document).on("click", ".uib_w_43", function(evt)
+    {
+         activate_subpage("#shee7"); 
+    });
+    
+        /* graphic button  صفار */
+    $(document).on("click", ".uib_w_44", function(evt)
+    {
+         activate_subpage("#s9far"); 
+    });
+    
+        /* graphic button  عتر */
+    $(document).on("click", ".uib_w_106", function(evt)
+    {
+         activate_subpage("#a3ter"); 
+    });
+    
+        /* graphic button  عرفج */
+    $(document).on("click", ".uib_w_107", function(evt)
+    {
+         activate_subpage("#a3rfg"); 
+    });
+    
+        /* graphic button  عشرق */
+    $(document).on("click", ".uib_w_108", function(evt)
+    {
+         activate_subpage("#a3shrq"); 
+    });
+    
+        /* graphic button  .uib_w_147 */
+    $(document).on("click", ".uib_w_147", function(evt)
+    {
+        activate_subpage("#nbatgrid");  
+    });
+    
+        /* graphic button  .uib_w_146 */
+    
+    
+        /* graphic button  .uib_w_148 */
+    $(document).on("click", ".uib_w_148", function(evt)
+    {
+         activate_subpage("#about"); 
+    });
+    
+        /* button  الصور */
+    
+    
+        /* graphic button  .uib_w_146 */
+    $(document).on("click", ".uib_w_146", function(evt)
+    {
+         activate_subpage("#imag"); 
+    });
+    
+        /* button  الصور */
+    
+    
+        /* button  الصور */
+    
+    
+        /* button  الصور */
+    $(document).on("click", ".uib_w_3", function(evt)
+    {
+         activate_subpage("#imag"); 
+    });
+    
+        /* button  الصور */
+    $(document).on("click", ".uib_w_3", function(evt)
+    {
+         activate_subpage("#about"); 
+    });
+    
+        /* button  الصور */
+    $(document).on("click", ".uib_w_150", function(evt)
+    {
+         activate_subpage("#imag"); 
+    });
+    
+    }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
